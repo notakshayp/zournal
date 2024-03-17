4f1b6ec717ccb0004d0880ab246e47558e1526b0
Akshay <notakshayp@gmail.com>
2024-03-17 11:03:41 +0530

    component for landing page

--- zournal/src/app/app.routes.ts
+++ zournal/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { LandingpageComponent } from '../components/landingpage/landingpage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '**', component: LandingpageComponent }
+];
